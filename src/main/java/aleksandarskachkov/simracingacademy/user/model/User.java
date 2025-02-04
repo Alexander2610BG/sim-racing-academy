42cea7ba0cbdad8e2ff11f81943c95814417cb2d
Alexandear2610BG <aleksandurskachkov@gmail.com>
2025-02-04 20:47:05 +0200

    Creating Entities

--- src/main/java/aleksandarskachkov/simracingacademy/user/model/User.java
+++ src/main/java/aleksandarskachkov/simracingacademy/user/model/User.java
@@ -1,4 +1,68 @@
 package aleksandarskachkov.simracingacademy.user.model;
 
+import aleksandarskachkov.simracingacademy.subscription.model.Subscription;
+import aleksandarskachkov.simracingacademy.track.model.Track;
+import aleksandarskachkov.simracingacademy.wallet.model.Wallet;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String profilePicture;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Country country;
+
+    @Column(nullable = false)
+    private boolean isActive;
+
+    @Column(nullable = false)
+    private LocalDateTime createdOn;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedOn;
+
+    @OneToOne(mappedBy = "owner")
+    private Wallet wallet;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "owner")
+    @OrderBy("createdOn DESC")
+    private List<Subscription> subscriptions = new ArrayList<>();
+
+    @ManyToMany
+    private List<Track> tracks = new ArrayList<>();
+
 }
